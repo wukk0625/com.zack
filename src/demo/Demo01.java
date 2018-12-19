589c159a8ff9adc7fa1cbfa60b8b5caf7f9f13ff
wukk0625 <33949553+wukk0625@users.noreply.github.com>
2018-12-19 22:30:16 +0800

    Update Demo01.java

--- src/demo/Demo01.java
+++ src/demo/Demo01.java
@@ -2,6 +2,6 @@ package demo;
 
 public class Demo01 {
     public static void main(String[] args) {
-        System.out.println("my name is Jack");
+        System.out.println("my name is MARRY");
     }
 }
