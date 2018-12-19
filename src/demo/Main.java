d0c6fddcb03145e6e4c28654883ae714cb91bb49
wukk0625 <33949553+wukk0625@users.noreply.github.com>
2018-12-19 22:38:33 +0800

    Update Main.java

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -2,6 +2,6 @@ package demo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world HHH");
     }
 }
