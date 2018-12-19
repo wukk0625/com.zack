df5fab938e9ef8f3742ec48c44da1d1f6032c625
wukk0625 <1114348819@qq.com>
2018-12-19 22:26:37 +0800

    my first commit in idea

--- src/demo/Demo01.java
+++ src/demo/Demo01.java
@@ -2,6 +2,6 @@ package demo;
 
 public class Demo01 {
     public static void main(String[] args) {
-        System.out.println("my name is tom");
+        System.out.println("my name is wukkaAAA");
     }
 }
