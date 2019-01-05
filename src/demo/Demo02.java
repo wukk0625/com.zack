9d3e780a205b05086c55a5e81018cdadf11a9949
wukk0625 <33949553+wukk0625@users.noreply.github.com>
2019-01-05 19:07:40 +0800

    Update Demo02.java

--- src/demo/Demo02.java
+++ src/demo/Demo02.java
@@ -2,6 +2,6 @@ package demo;
 
 public class Demo02 {
     public static void main(String[] args) {
-        System.out.println("my name is HH");
+        System.out.println("my name is FQ");
     }
 }
